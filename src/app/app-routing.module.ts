6539162e4dea12dce0e83234be3f94db628fbba5
James Patterson <jamespatterson.feswe@yahoo.com>
2024-01-28 14:28:15 -0500

    add: start using the sale banner and footer in app html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent
+    // loadChildren: () =>
+    //   import('./app.module')
+    //   .then(module => module.AppModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterModule, SaleBannerModule } from '@jamespatterson-feswe/components';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    // BrowserAnimationsModule,
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    FooterModule,
+    SaleBannerModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
